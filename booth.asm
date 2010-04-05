debe987eef611bef07ff5463f863bbd17e524590
hckirmizi <hckirmizi@70fffba1-c0bd-17ed-2589-6b2c7d1f6353>
2010-04-05 12:38:59 +0000

    text arrangement test.

--- booth.asm
+++ booth.asm
@@ -1,10 +1,10 @@
 #############################################################################
 #
 # Program Name		: booth.asm
-# Author			: Hakki Caner KIRMIZI
+# Author		: Hakki Caner KIRMIZI
 # Description		: Booth Algorithm implementation in MIPS
 # Environment		: Windows-7 Entreprise
-# Simulator			: MARS 3.8 (MIPS Assembler and Runtime Simulator)
+# Simulator		: MARS 3.8 (MIPS Assembler and Runtime Simulator)
 # Version Control	: TortoiseSVN 1.6.7, Build 18415 - 32 Bit
 # Project Hosting	: https://code.google.com/p/mips-booth-multiplication/
 #
@@ -19,13 +19,13 @@ str_please_enter_int:	.asciiz "Please enter an integer: "
 str_you_entered_int:	.asciiz "You entered: "
 
 # Syscall variables
-sys_print_int:			.word 1
-sys_print_string:		.word 4
-sys_read_int:			.word 5
-sys_exit:				.word 10
+sys_print_int:		.word 1
+sys_print_string:	.word 4
+sys_read_int:		.word 5
+sys_exit:		.word 10
 
 # Local variables
-i:						.word
+i:			.word
 
 
 # ====== TEXT SEGMENT ====== #
@@ -39,7 +39,7 @@ main:
 	syscall
 
 	# get integer into i
-	lw  $v0,  sys_read_int
+	lw  $v0, sys_read_int
 	syscall
 	la  $t0, i
 	sw  $v0,  ($t0)
@@ -55,5 +55,5 @@ main:
 	syscall
 
 	# Exit
-	lw  $v0,  sys_exit
+	lw  $v0, sys_exit
 	syscall
