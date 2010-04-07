442e70d59322b1365b1fc701db4e02a7d366cd7d
hckirmizi <hckirmizi@70fffba1-c0bd-17ed-2589-6b2c7d1f6353>
2010-04-07 15:01:22 +0000

    some comments fixed.

--- booth.asm
+++ booth.asm
@@ -5,13 +5,14 @@
 # Description		: Booth Algorithm implementation in MIPS
 # Environment		: Windows-7 Entreprise
 # Simulator		: MARS 3.8 (MIPS Assembler and Runtime Simulator)
+# Editor		: Vim 7.2 for Win
 # Version Control	: TortoiseSVN 1.6.7, Build 18415 - 32 Bit
 # Project Hosting	: https://code.google.com/p/mips-booth-multiplication/
 #
 # Notes			:
 # -----------------------
 # 1) Algorithm will work better, if the number who has 'less bit transitions' is 
-#    initialized to X(multiplicand).
+#    initialized to X(as multiplicand).
 # 2) Here some good explanations of the algorithm with examples:
 #    http://ftp.csci.csusb.edu/schubert/tutorials/csci313/w04/TL_Booth.pdf
 #    http://ftp.csci.csusb.edu/schubert/tutorials/csci313/w04/TB_BoothTutorial.pdf
@@ -23,8 +24,8 @@
 # $s3 = U --> holds the results from each step in the algorithm
 # $s4 = V --> holds the overflow from U, when right-shift
 # $s5 = X-1 --> holds the least significant bit from X before each right-shift
-# $s6 = N --> an extra bit to the left of U to be able to perform multiplication
-# 	      when multiplicand is the largest negative number
+# $s6 = N --> an extra bit to the left of U to perform multiplication
+# 	      when multiplier is the largest negative number
 #
 ###################################################################################
 
@@ -66,8 +67,8 @@ sys_exit:			.word 10
 	.globl main
 
 
-#	main of the program -initialization and accept inputs
-#	-----------------------------------------------------
+#	main of the program -initializations and accept inputs
+#	------------------------------------------------------
 
 main:
 	# initialize loop counter = 0, U=0, V=0, X-1=0, N=0
@@ -189,84 +190,84 @@ print_step:
 #	-----------------------------------------------------------------
 	
 	andi $t0, $s1, 1		# $t0 = LSB of X
-	beq  $t0, $zero, x_lsb_0	# if ($t0 == 0)
-	j    x_lsb_1			# otherwise
+	beq  $t0, $zero, x_lsb_0	# if ($t0 == 0) then goto x_lsb_0
+	j    x_lsb_1			# if ($t1 == 1) then goto x_lsb_1
 
 x_lsb_0: 				# when the LSB of X = 0
-	beq  $s5, $zero, case_00	# if (X-1 == 0) then goto "X=0 & X-1=0" case
-	j    case_01			# otherwise goto "X=0 & X-1=1" case
+	beq  $s5, $zero, case_00	# if (X-1 == 0) then goto case_00
+	j    case_01			# if (X-1 == 1) then goto case_01
 
 x_lsb_1:				# when the LSB of X = 1
-	beq  $s5, $zero, case_10	# if (X-1 == 0) then goto "X=1 & X-1=0" case
-	j    case_11			# otherwise goto "X=1 & X-1=1" case
+	beq  $s5, $zero, case_10	# if (X-1 == 0) then goto case_10
+	j    case_11			# if (X-1 == 1) then goto case_11
 
 case_00:
-	# print info
+	# print info about action
 	lw   $v0, sys_print_string
 	la   $a0, str_print_00_info
 	syscall
-	# do nothing, but shift
+	# do nothing, but shifting
 	andi $s6, $s6, 0		# set N=0 anyway
 	andi $t0, $s3, 1		# LSB of U for overflow checking
-	bne  $t0, $zero, V		# if LSB of U not zero, goto update 
-	srl  $s4, $s4, 1		# shift right logical V by 1 bit
-	j    shift
+	bne  $t0, $zero, V		# if LSB of U not zero, goto V, i.e. U overflows
+	srl  $s4, $s4, 1		# shift right logical V by 1-bit
+	j    shift			# goto shift other variables
 
 case_01:
-	# print info
+	# print info about action
 	lw   $v0, sys_print_string
 	la   $a0, str_print_01_info
 	syscall
-	# do addition and shift
+	# do addition and shifting
 	add  $s3, $s3, $s2		# add Y to U
 	andi $s5, $s5, 0		# X=0, so next time X-1=0
 	ori  $s6, $s6, 1		# X-1=1, so next time N=1
 	andi $t0, $s3, 1		# LSB of U for overflow checking
-	bne  $t0, $zero, V		# if LSB of U not zero, goto update V
-	srl  $s4, $s4, 1		# shift right logical V by 1 bit
-	j    shift
+	bne  $t0, $zero, V		# if LSB of U not zero, goto V, i.e. U overflows
+	srl  $s4, $s4, 1		# shift right logical V by 1-bit
+	j    shift			# goto shift other variables
 
 case_10:
-	# print info
+	# print info about action
 	lw   $v0, sys_print_string
 	la   $a0, str_print_10_info
 	syscall
-	# do subtract and shift
+	# do subtract and shifting
 	sub  $s3, $s3, $s2		# sub Y from U
 	ori  $s5, $s5, 1		# X=1, so next time X-1=1
 	andi $s6, $s6, 0		# X-1=0, so next time N=0
 	andi $t0, $s3, 1		# LSB of U for overflow checking
 	bne  $t0, $zero, V		# if LSB of U not zero, goto update V
-	srl  $s4, $s4, 1		# shift right logical V by 1 bit
-	j    shift
+	srl  $s4, $s4, 1		# shift right logical V by 1-bit
+	j    shift			# goto shift other variables
 
 case_11:
-	# print info
+	# print info about action
 	lw   $v0, sys_print_string
 	la   $a0, str_print_11_info
 	syscall
-	# do nothing, but shift
+	# do nothing, but shifting
 	ori  $s6, $s6, 1		# set N=1 anyway
 	andi $t0, $s3, 1		# LSB of U for overflow checking
 	bne  $t0, $zero, V		# if LSB of U not zero, goto update
-	srl  $s4, $s4, 1		# shift right logical V by 1 bit
-	j    shift 
+	srl  $s4, $s4, 1		# shift right logical V by 1-bit
+	j    shift 			# goto shift other variables
 
 V:
 	andi $t0, $s4, 0x80000000	# What is the MSB of V?
 	bne  $t0, $zero, shiftV		# If MSB == 1, goto shiftV
-	srl  $s4, $s4, 1		# MSB == 0, so first shift right logical V by 1
+	srl  $s4, $s4, 1		# MSB == 0, so first shift right logical V by 1-bit
 	ori  $s4, $s4, 0x80000000	# then make MSB of V = 1
-	j    shift
+	j    shift			# goto shift other variables
 
 shiftV:
-	srl  $s4, $s4, 1
+	srl  $s4, $s4, 1		# shift right logical V by 1-bit
 	ori  $s4, $s4, 0x80000000	# MSB 0f V = 1
-	j    shift
+	j    shift			# goto shift other variables
 
 shift:
-	sra  $s3, $s3, 1		# shift right arithmetic U by 1 bit
-	ror  $s1, $s1, 1		# rotate right X by 1 bit
+	sra  $s3, $s3, 1		# shift right arithmetic U by 1-bit
+	ror  $s1, $s1, 1		# rotate right X by 1-bit
 	addi $s0, $s0, 1		# decrement loop counter
 	beq  $s0, 32, save		# if it is last step, save the contents of the regs for result
 	j    print_step			# loop again
@@ -275,6 +276,7 @@ save:
 	add  $t1, $zero, $s3		# save U in $t1
 	add  $t2, $zero, $s4		# save V in $t2
 	j    print_step			# loop again
+
 
 #	exit -calculation completed, so print result
 #	--------------------------------------------
